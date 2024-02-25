e134453f30221c9f0f2a84d59e48fa67426063c1
javi <javicortez@protonmail.com>
2024-02-25 02:00:52 -0700

    Update size_category to dropdown menu in Sneaker form

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,4 +1,4 @@
 class Sneaker < ApplicationRecord
-    validates :size, numericality: { greater_than: 0, less_than: 100 }
-
+ validates :size, numericality: { greater_than: 0, less_than: 100 }
+ validates :size_category, inclusion: { in: %w(M W GS PS TD) }   
 end
